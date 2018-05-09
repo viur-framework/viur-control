15dea5df5255c3458abb1659d4794682e2c860c4
MT <mtemimi@gmail.com>
2018-05-09 11:35:06 +0200

    - Updated Node.js download path

--- assets/dependency-installer/windows/install_nodejs.bat
+++ assets/dependency-installer/windows/install_nodejs.bat
@@ -2,7 +2,7 @@
 
 ECHO Installing NodeJS ...
 
-start /wait msiexec /quiet /passive /i "https://nodejs.org/dist/v9.2.0/node-v9.2.0-x64.msi"
+start /wait msiexec /quiet /passive /i "https://nodejs.org/dist/v10.0.0/node-v10.0.0-x64.msi"
 "C:\Program Files\nodejs\npm.cmd" install -g less
 
 ECHO Adding npm to your path environment variable ...
