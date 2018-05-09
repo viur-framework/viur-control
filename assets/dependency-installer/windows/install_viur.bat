cffdf27987a02890453ea04cbd4368b8509f404c
MT <mtemimi@gmail.com>
2018-05-09 11:46:48 +0200

    - Fixed ViUR Control clone url

--- assets/dependency-installer/windows/install_viur.bat
+++ assets/dependency-installer/windows/install_viur.bat
@@ -2,6 +2,6 @@
 
 ECHO Downloading ViUR ...
 
-"C:\Program Files\Git\bin\git.exe" clone "https://github.com/melectrik/viur-framework_control.git" "%USERPROFILE%\viur-control"
+"C:\Program Files\Git\bin\git.exe" clone "https://github.com/viur-framework/control.git" "%USERPROFILE%\viur-control"
 
 
