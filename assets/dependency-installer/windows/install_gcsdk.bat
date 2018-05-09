bc5e36494d5dc2969c94d0ae63f77af2b858af34
MT <mtemimi@gmail.com>
2018-05-09 11:09:01 +0200

    - Minor fix

--- assets/dependency-installer/windows/install_gcsdk.bat
+++ assets/dependency-installer/windows/install_gcsdk.bat
@@ -7,7 +7,7 @@ ECHO Extracting Google SDK installer ...
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%USERPROFILE%\GoogleCloudSDKInstaller.zip', '%USERPROFILE%\google-cloud-sdk-installer'); }"
 
 xcopy /E /H /K /Q /Y "%USERPROFILE%\google-cloud-sdk-installer" "%USERPROFILE%\"
-"%USERPROFILE%\google-cloud-sdk\install.bat" --quiet --usage-reporting true --path-update true --additional-components beta app-engine-python app-engine-python-extras
+CALL "%USERPROFILE%\google-cloud-sdk\install.bat" --quiet --usage-reporting true --path-update true --additional-components beta app-engine-python app-engine-python-extras
 
 ECHO Adding Google SDK to your system path environment variable ...
 setx path "%PATH%;%USERPROFILE%\google-cloud-sdk\bin"
